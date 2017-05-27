dd481c7f6d34c95312b52231a307e3a28d14ca63
Rizwan Nawab <nawabrizwan7@gmail.com>
2017-05-27 13:49:57 +0530

    Add files via upload

---
+++ MadridMatch.py
@@ -0,0 +1,60 @@
+import requests
+import datetime
+from bs4 import BeautifulSoup as bs
+from lxml import html
+
+url = 'http://www.realmadrid.com/en/football/schedule'
+response = requests.get(url)
+html = response.content
+soup = bs(html,"lxml")
+
+#getting contents from Real madrid website
+opp1 = soup.find('div', {'class': 'm_highlighted_next_game_team'}).strong.contents
+opp2 = soup.find('div', {'class': 'm_highlighted_next_game_team m_highlighted_next_game_second_team'}).strong.contents
+time = soup.find('div', {'class': 'm_highlighted_next_game_info_wrapper'}).time.contents
+date = soup.find('header', {'class': 'm_highlighted_next_game_header'}).time.contents
+location = soup.find('p', {'class': 'm_highlighted_next_game_location'}).contents
+matchtype = soup.find('header', {'class': 'm_highlighted_next_game_header'}).span.contents
+
+#Storing the values
+time1 = time[0].strip()
+hour=int(time1[:2])
+mins = int(time1[3:5])
+date1 = date[0].strip()
+year=int(date1[:4])
+month=date1[5:7]
+if '0' in month:
+    month=month[-1:]
+month=int(month)
+day=int(date1[-2:])
+
+#Converting to IST
+hour = hour+3
+mins = mins+30
+if mins>60:
+    hour = hour+1
+    mins = mins-60
+if hour>=24:
+    hour = 00
+    day = day+1
+
+
+"""Printing match details"""
+print (matchtype[0],"\n",location[0])
+print(opp1[0],"(H) vs ",opp2[0],"(A) \n",str(hour).zfill(2),":",str(mins)," on ",str(day),"/",str(month),"/",str(year))
+
+#Countdown
+import time
+def countdown(t):
+    while t:
+        min1, secs = divmod(t, 60)
+        hours1, mins = divmod(min1,60)
+        days, hours = divmod(hours1, 24)
+        timeformat = '{:02d} days {:02d}:{:02d}:{:02d}'.format(days,hours, mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
+start = datetime.datetime.now()
+end = datetime.datetime(year=year, month=month, day=day, hour=hour)
+diff = end - start
+countdown(int(diff.total_seconds()))
